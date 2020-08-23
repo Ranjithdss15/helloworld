f7c3d2b78c920c8e8a637f736c7dc8cfdbdd6de2
Camillus Ranjith Doss <cpaul@altimetrik.com>
2020-08-23 12:15:59 +0530

    Disable git cred

--- jenkins/JenkinsfileBuild.groovy
+++ jenkins/JenkinsfileBuild.groovy
@@ -4,7 +4,7 @@ final GIT_REPO = 'https://github.com/Ranjithdss15/helloworld.git'
 
 final ECR_REGISTRY = "${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/${STACKNAME}"
 final DEPLOY_ROLE_ARN = "arn:aws:iam::${ACCOUNT}:role/${STACKNAME}-deploy"
-final GIT_CREDENTIALS_ID = "bitbucket_access"
+// final GIT_CREDENTIALS_ID = "bitbucket_access"
 ​
 def gitBranch
 def version
