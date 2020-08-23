f7ae60f39ee78002837cc329d964b47fb9d30a05
Camillus Ranjith Doss <cpaul@altimetrik.com>
2020-08-23 19:50:44 +0530

    Testing

--- jenkins/JenkinsfileBuild.groovy
+++ jenkins/JenkinsfileBuild.groovy
@@ -1,13 +1,6 @@
-final STACKNAME = 'helloworld-node'
-final ACCOUNT = '730186024834'
-final GIT_REPO = 'https://github.com/Ranjithdss15/helloworld.git'
 
-final ECR_REGISTRY = "${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/${STACKNAME}"
-final DEPLOY_ROLE_ARN = "arn:aws:iam::${ACCOUNT}:role/${STACKNAME}-deploy"
-final GIT_CREDENTIALS_ID = "1737ad6b-0599-4841-acc1-c42083ca226e"
 ​
-def gitBranch
-def version
+
 ​
 node ('master') {
 ​
@@ -15,7 +8,8 @@ node ('master') {
         timestamps {
             print 'Prepping workspace for build...'
         }
-​
+​        def gitBranch
+        def version
         deleteDir()
 ​
         gitBranch = env.BRANCH_NAME ?: 'master'
@@ -52,6 +46,13 @@ node ('master') {
             timestamps {
                 print 'Building and publishing our container...'
             }
+            final STACKNAME = 'helloworld-node'
+            final ACCOUNT = '730186024834'
+            final GIT_REPO = 'https://github.com/Ranjithdss15/helloworld.git'
+
+            final ECR_REGISTRY = "${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/${STACKNAME}"
+            // final DEPLOY_ROLE_ARN = "arn:aws:iam::${ACCOUNT}:role/${STACKNAME}-deploy"
+            final GIT_CREDENTIALS_ID = "1737ad6b-0599-4841-acc1-c42083ca226e"
 ​
             sh "docker build -t ${STACKNAME} ."
             sh "docker tag ${STACKNAME}:latest ${ECR_REGISTRY}:${version}"
