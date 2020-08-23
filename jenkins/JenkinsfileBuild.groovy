a8cf0207534976eae9952bfcdb930d41f37ddba5
Camillus Ranjith Doss <cpaul@altimetrik.com>
2020-08-23 20:07:10 +0530

    Testing

--- jenkins/JenkinsfileBuild.groovy
+++ jenkins/JenkinsfileBuild.groovy
@@ -5,7 +5,7 @@
 // final ECR_REGISTRY = "${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/${STACKNAME}"
 // // final DEPLOY_ROLE_ARN = "arn:aws:iam::${ACCOUNT}:role/${STACKNAME}-deploy"
 // final GIT_CREDENTIALS_ID = "1737ad6b-0599-4841-acc1-c42083ca226e"
-​node("master"){
+​node{
     stage("test 1"){
         sh "docker images"
     }
